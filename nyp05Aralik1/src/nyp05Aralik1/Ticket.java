82b1f8a0e80079f0f1def154f2c5310d45dae3f0
blgyemret6334-ops <243034060+blgyemret6334-ops@users.noreply.github.com>
2026-01-05 23:00:49 +0300

    Her şey fatih için

--- nyp05Aralik1/src/nyp05Aralik1/Ticket.java
+++ nyp05Aralik1/src/nyp05Aralik1/Ticket.java
@@ -5,90 +5,10 @@ import java.io.Serializable;
 
 
 public class Ticket implements Serializable {
-    private String ticketID;
-    private Reservation reservation;
+	
+	private static final long serialVersionUID = 1L;
+	private String ticketID;
+	private Reservation reservation;
     private double price;
-    private Baggage baggageAllowance;
-
-    public Ticket(String ticketID, double price, Baggage baggageAllowance) {
-        this.ticketID = ticketID;package flightManagementModule;
-        import java.io.Serializable;
-
-
-        public class Ticket implements Serializable {
-            private String ticketID;
-            private Reservation reservation;
-            private double price;
-            private Baggage baggageAllowance;
-
-            public Ticket(String ticketID, Reservation reservation, double price, Baggage baggageAllowance) {
-                this.ticketID = ticketID;
-                this.reservation = reservation;
-                this.price = price;
-                this.baggageAllowance = baggageAllowance;
-            }
-
-
-            public String getTicketID() {
-                return ticketID;
-            }
-
-
-            public Reservation getReservation() {
-                return reservation;
-            }
-
-
-            public double getPrice() {
-                return price;
-            }
-
-
-            public Baggage getBaggageAllowance() {
-                return baggageAllowance;
-            }
-
-            @Override
-            public String toString() {
-                return "Ticket ID: " + ticketID +
-                        "\nPrice: " + price +
-                        "\nBaggage: " + baggageAllowance.getWeight() + "kg" +
-                        "\n--- Reservation Details ---\n" + reservation.toString();
-            }
-
-
-        }
-        this.price = price;
-        this.baggageAllowance = baggageAllowance;
-    }
-
-
-    public String getTicketID() {
-        return ticketID;
-    }
-
-
-    public Reservation getReservation() {
-        return reservation;
-    }
-
-
-    public double getPrice() {
-        return price;
-    }
-
-
-    public Baggage getBaggageAllowance() {
-        return baggageAllowance;
-    }
-
-    @Override
-    public String toString() {
-        return "Ticket ID: " + ticketID +
-                "\nPrice: " + price +
-                "\nBaggage: " + baggageAllowance.getWeight() + "kg" +
-                "\n--- Reservation Details ---\n" + reservation.toString();
-    }
-
-
+    private int baggageAllowance;
 }
